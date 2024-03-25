f21774745da582beea1a05732ba7ff5b1b894016
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-03-26 01:08:28 +0530

    m

--- src/home/navbarTeam.js
+++ src/home/navbarTeam.js
@@ -80,12 +80,12 @@ const NavBarTeam = () => {
                 </div>
             </div>
             <div className="menu-toggle" onClick={toggleNav}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <line x1="3" y1="12" x2="21" y2="12"></line>
-                    <line x1="3" y1="6" x2="21" y2="6"></line>
-                    <line x1="3" y1="18" x2="21" y2="18"></line>
-                </svg>
-            </div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <line x1="3" y1="12" x2="21" y2="12"></line>
+        <line x1="3" y1="6" x2="21" y2="6"></line>
+        <line x1="3" y1="18" x2="21" y2="18"></line>
+    </svg>
+</div>
         </div>
     );
 }
