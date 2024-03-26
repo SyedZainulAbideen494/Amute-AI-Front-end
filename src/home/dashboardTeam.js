5f85d53011f77d73d37f217bd5e73cb2e48db052
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-03-26 14:47:45 +0530

    m

--- src/home/dashboardTeam.js
+++ src/home/dashboardTeam.js
@@ -2,12 +2,16 @@ import React from "react";
 import './home.css';
 import notificationicon from '../images/icons8-notifications-64.png';
 import settingsicon from '../images/icons8-settings-50.png'
+import { Link } from "react-router-dom";
 
 const DashboardTeam = () => {
     return (
         <div className="dashboard_team_main_div">
             <div className="dashvoard_team_header">
                 <h3>Dashboard</h3>
+                <Link to='/summary'>
+                <button className="mobl_btn_dashboard_btn_summary">Summary</button>
+                </Link>
                 <button className="setting_btn_dashboard_team"><img src={settingsicon} alt="Settings" className="notification_dashboard_icon" /></button>
                 <button className="notification_icon"><img src={notificationicon} alt="Notifications" className="notification_dashboard_icon" /></button>
             </div>
---
+++ src/home/summary.js
@@ -0,0 +1,8 @@
+import React,{useCallback} from "react";
+import RightMenuSummary from "./rightmenuTeam";
+
+const Summary = () => {
+    return<RightMenuSummary/>
+}
+
+export default Summary
