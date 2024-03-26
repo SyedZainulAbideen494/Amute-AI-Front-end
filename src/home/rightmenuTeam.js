937667468227728e8e8d5eba2ea791962de0d25f
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-03-26 12:22:10 +0530

    m

--- src/home/rightmenuTeam.js
+++ src/home/rightmenuTeam.js
@@ -4,7 +4,9 @@ import { API_ROUTES } from "../app-modules/api_routes";
 
 const RightMenuTeam = () => {
     return<div className="rightmenu_team_main_div">
-        
+        <div className="right_menu_Header">
+        <h2>Summary</h2>
+        </div>
     </div>
 }
 
