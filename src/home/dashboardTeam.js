5328b7822b19e28915c3203257879dd3e4490957
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-03-26 12:52:47 +0530

    m

--- src/home/dashboardTeam.js
+++ src/home/dashboardTeam.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './home.css';
 import notificationicon from '../images/icons8-notifications-64.png';
+import settingsicon from '../images/icons8-settings-50.png'
 
 const DashboardTeam = () => {
     return (
         <div className="dashboard_team_main_div">
             <div className="dashvoard_team_header">
                 <h3>Dashboard</h3>
-                <button className="setting_btn_dashboard_team">Settings</button>
+                <button className="setting_btn_dashboard_team"><img src={settingsicon} alt="Settings" className="notification_dashboard_icon" /></button>
                 <button className="notification_icon"><img src={notificationicon} alt="Notifications" className="notification_dashboard_icon" /></button>
             </div>
         </div>
