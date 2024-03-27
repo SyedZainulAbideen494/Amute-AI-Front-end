693e3c458d22c9755973e217b4b8d2e8a8279a0f
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-03-27 17:15:48 +0530

    m

--- src/team/createTeam.js
+++ src/team/createTeam.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { API_ROUTES } from '../app-modules/api_routes';
-import './team.css'
+import './team.css';
 
 const CreateTeam = () => {
   const [teamName, setTeamName] = useState('');
   const [userCompanies, setUserCompanies] = useState([]);
   const [selectedCompany, setSelectedCompany] = useState('');
   const [isLoading, setIsLoading] = useState(true);
+  const [successMsg, setSuccessMsg] = useState('');
 
   useEffect(() => {
     // Fetch user's companies
@@ -37,30 +38,38 @@ const CreateTeam = () => {
       return;
     }
 
-    const response = await axios.post(API_ROUTES.getUserId, { token });
-    const userId = response.data.userId;
-
-    // Insert team details into the database
-    const teamData = { name: teamName, companyId: selectedCompany, userId: userId };
     try {
+      const response = await axios.post(API_ROUTES.getUserId, { token });
+      const userId = response.data.userId;
+
+      // Insert team details into the database
+      const teamData = { name: teamName, companyId: selectedCompany, userId: userId };
       const teamResponse = await axios.post(API_ROUTES.createTeam, teamData);
       console.log('Team created:', teamResponse.data);
+
+      // Show success message
+      setSuccessMsg('Team created successfully!');
+
+      // Clear input fields
+      setTeamName('');
+      setSelectedCompany('');
     } catch (error) {
       console.error('Error creating team:', error);
     }
   };
 
   return (
-    <div className="create-team-container"> {/* Apply container class */}
-    <input type="text" placeholder="Team Name" value={teamName} onChange={(e) => setTeamName(e.target.value)} />
-    <select value={selectedCompany} onChange={(e) => setSelectedCompany(e.target.value)}>
-      <option value="">Select Company</option>
-      {userCompanies.map(company => (
-        <option key={company.id} value={company.id}>{company.name}</option>
-      ))}
-    </select>
-    <button onClick={handleCreateTeam}>Create Team</button>
-  </div>
+    <div className="create-team-container">
+      <input type="text" placeholder="Team Name" value={teamName} onChange={(e) => setTeamName(e.target.value)} />
+      <select value={selectedCompany} onChange={(e) => setSelectedCompany(e.target.value)}>
+        <option value="">Select Company</option>
+        {userCompanies.map(company => (
+          <option key={company.id} value={company.id}>{company.name}</option>
+        ))}
+      </select>
+      <button onClick={handleCreateTeam}>Create Team</button>
+      {successMsg && <p className="success-msg">{successMsg}</p>}
+    </div>
   );
 };
 
