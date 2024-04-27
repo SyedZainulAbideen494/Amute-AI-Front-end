45cb18230a28dce6f59a1b41e3a0eb7ddf22a36e
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-04-27 21:17:29 +0530

    m

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import HomeTeam from "./home/homeTeam";
 import Signinform from "./auth/signinform";
 import Loginform from "./auth/loginform";
 import Website from "./website/website";
+import Summary from "./home/summary";
 
 const router = createBrowserRouter([
   { path: "/dashboard", element: <HomeTeam /> },
   { path: '/', element: <Website/>},
   { path: "/signup", element: <Signinform/>},
   { path: "/login", element: <Loginform/>},
+  { path: 'summary', element: <Summary/> },
 ]);
 
 
