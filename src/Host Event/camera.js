3813ed6be2c9b4e71a289841ecd8850eb1853e7d
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-04-28 12:55:42 +0530

    m

--- src/Host Event/camera.js
+++ src/Host Event/camera.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import qrReaderRef from 'react-qr-reader';
+
 import './hostEvent.css'; // Import CSS file for styling
 
 const QRScanner = () => {
@@ -18,14 +18,7 @@ const QRScanner = () => {
 
   return (
     <div className="qr-scanner-container">
-      <qrReaderRef
-        ref={qrReaderRef}
-        delay={300}
-        onError={handleError}
-        onScan={handleScan}
-        style={{ width: '100%', height: '100%' }}
-        facingMode="environment" // Use the back camera
-      />
+     
       {result && (
         <div className="qr-result-box">
           <p>{result}</p>
