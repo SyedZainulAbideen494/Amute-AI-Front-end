cb4f84fa5ba2b45d5588965284246e67db4e1c9e
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-04-29 16:22:43 +0530

    m

--- src/QR Code Scanner/qrscanner.js
+++ src/QR Code Scanner/qrscanner.js
@@ -1,53 +1,30 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-scanner';
 
 const QRScanner = () => {
-  const videoRef = useRef(null);
-  const [scanning, setScanning] = useState(false);
   const [result, setResult] = useState('');
 
-  const startScanner = () => {
-    setScanning(true);
-    setResult('');
-
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
-      .then(stream => {
-        videoRef.current.srcObject = stream;
-        videoRef.current.play();
-      })
-      .catch(error => {
-        console.error('Error accessing camera:', error);
-      });
-  };
-
-  const stopScanner = () => {
-    setScanning(false);
-    const stream = videoRef.current.srcObject;
-    if (stream) {
-      const tracks = stream.getTracks();
-      tracks.forEach(track => track.stop());
+  const handleScan = data => {
+    if (data) {
+      setResult(data);
     }
-  };
+  }
 
-  const handleScan = (event) => {
-    const code = event.target.value;
-    setResult(code);
-    stopScanner();
-  };
+  const handleError = err => {
+    console.error(err);
+  }
 
   return (
     <div>
-      {!scanning ? (
-        <button onClick={startScanner}>Start Scan</button>
-      ) : (
-        <div>
-          <video ref={videoRef} style={{ width: '100%' }} onScan={handleScan}></video>
-          <button onClick={stopScanner}>Stop Scan</button>
-        </div>
-      )}
-      <h2>Hello</h2>
-      {result && <p style={{color: 'white'}}>Scanned QR code: {result}</p>}
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '100%' }}
+      />
+      {result && <p>Scanned QR code: {result}</p>}
     </div>
   );
-};
+}
 
 export default QRScanner;
