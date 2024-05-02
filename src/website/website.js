439efa43f04d23f3e2214ef2aba3abca67d7d6d4
SyedZainulAbideen494 <zaonkaleem27@gmail.com>
2024-05-02 17:53:19 +0530

    m

--- src/website/website.js
+++ src/website/website.js
@@ -10,6 +10,8 @@ const Website = () => {
       if (token) {
         // Token is present, redirect to '/dashboard'
         navigate('/dashboard');
+      }else {
+        navigate('/dashboard')
       }
       // If token is not present, do nothing
     }, [navigate]); // Ensure to include navigate in the dependency array
